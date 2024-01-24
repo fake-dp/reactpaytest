9311b08512c42f9dbed9cd8203547c40c184c3c0
fake-dp <fakecoko92@gmail.com>
2024-01-24 17:55:07 +0900

    web test

---
+++ src/page/TestData.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+function TestData(props) {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const handleEvent = (event) => {
+            // 여기서 event.data에는 React Native에서 보낸 데이터가 포함되어 있습니다.
+            const data = JSON.parse(event.data);
+            console.log('React Native로부터 받은 데이터: ', data);
+            setData(data);
+        };
+
+        window.addEventListener('message', handleEvent);
+
+        // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
+        return () => {
+            window.removeEventListener('message', handleEvent);
+        };
+    }, []);
+
+    return (
+        <div>
+            웹뷰 데이터 받아오기 test {JSON.stringify(data)}
+        </div>
+    );
+}
+
+export default TestData;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import PaymentDone from '../page/PaymentDone';
 import PaymentLink from '../page/PaymentLink';
 import Payment from '../page/Payment';
+import TestData from '../page/TestData';
 function AppRouter(props) {
     return (
     <Routes>
       <Route path='/paymentlink/:id' element={<PaymentLink />} />
       <Route path='/payment/complete' element={<PaymentDone />} />
-      <Route path='/payment' element={<Payment />} />
+      {/* <Route path='/payment' element={<Payment />} /> */}
+      <Route path='/payment' element={<TestData />} />
     </Routes>
     );
 }
