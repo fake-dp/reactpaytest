d95abbd238a02faaa96f606249055331707d0d1a
fake-dp <fakecoko92@gmail.com>
2024-01-25 10:27:28 +0900

    다시 데ㅣ터 받아오기

--- src/page/TestData.jsx
+++ src/page/TestData.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 function TestData(props) {
-    const [data, setData] = useState(null);
+    const [test, setTest] = useState(null);
 
     useEffect(() => {
         const handleEvent = (event) => {
             try {
                 // 여기서 event.data에는 React Native에서 보낸 데이터가 포함되어 있습니다.
                 const data = JSON.parse(event.data);
-                console.log('React Native로부터 받은 데이터: ', data);
-                setData(data);
+  
+                setTest(data);
             } catch(error) {
                 console.log('Received data is not valid JSON: ', event.data);
             }
@@ -24,17 +24,20 @@ function TestData(props) {
     }, []);
 
     return (
+        <>
         <div>
-        웹뷰 데이터 받아오기 test!!!!
-
-            <div>
-                <p>{data}</p>
-                {/* <p>paymentInfoData: {data.paymentInfoData}</p> */}
-                {/* <p>totalPrice: {data.totalPrice}</p> */}
-                {/* <p>goodsName: {data.goodsName}</p> */}
-            </div>
-
-    </div>
+                <h1>웹뷰 데이터 받아오기 @@#!</h1>
+        </div>
+        <div>
+            
+             
+                    <>
+                        <p>test: {test}</p>
+                    </>
+                
+            
+        </div>
+        </>
     );
 }
 
