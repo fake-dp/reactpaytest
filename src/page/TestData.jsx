22fc644dd29c52e00d872b2a91434f8681b42d4d
fake-dp <fakecoko92@gmail.com>
2024-01-25 12:17:47 +0900

    배포에러 해결

--- src/page/TestData.jsx
+++ src/page/TestData.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function TestData(props) {
@@ -14,8 +13,8 @@ function TestData(props) {
         const handleEvent = (event) => {
             try {
                 // 여기서 event.data에는 React Native에서 보낸 데이터가 포함되어 있습니다.
-                // const data = event.data?.paymentInfoData;
-                const data = JSON.parse(event.data.paymentInfoData);
+                const data = event.data;
+                // const data = JSON.parse(event.data.paymentInfoData);
                 setTest(data);
             } catch(error) {
                 console.log('Received data is not valid JSON: ', event.data);
