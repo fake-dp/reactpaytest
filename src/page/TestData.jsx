815226c65f5c157ad038b919d9ef88212480cf11
fake-dp <fakecoko92@gmail.com>
2024-01-25 12:15:24 +0900

    params확인용

--- src/page/TestData.jsx
+++ src/page/TestData.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function TestData(props) {
     const [test, setTest] = useState(null);
+    const location = useLocation();
+
+    const queryParams = new URLSearchParams(location.search);
+    const goodsName = queryParams.get('goodsName');
+    const totalPrice = queryParams.get('totalPrice');
 
     useEffect(() => {
         const handleEvent = (event) => {
             try {
                 // 여기서 event.data에는 React Native에서 보낸 데이터가 포함되어 있습니다.
-                const data = event.data?.paymentInfoData;
-                // const data = JSON.parse(event.data);
+                // const data = event.data?.paymentInfoData;
+                const data = JSON.parse(event.data.paymentInfoData);
                 setTest(data);
             } catch(error) {
                 console.log('Received data is not valid JSON: ', event.data);
@@ -32,7 +39,11 @@ function TestData(props) {
             
              
                     <>
-                        <p>test: {JSON.parse(test.paymentInfoData)}</p>
+                        <p>test: {test.paymentInfoData}</p>
+                        <p>
+                            <span>goodsName: {goodsName}</span>
+                            <span>totalPrice: {totalPrice}</span>
+                        </p>
                     </>
                 
             
