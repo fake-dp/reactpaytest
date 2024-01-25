84b3a58ee4d08e300e0da4b631a4c9eddff21bf4
fake-dp <fakecoko92@gmail.com>
2024-01-25 12:25:14 +0900

    다시 테스트

--- src/page/TestData.jsx
+++ src/page/TestData.jsx
@@ -1,13 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import CryptoJS from 'crypto-js';
+import { format } from 'date-fns';
 
 function TestData(props) {
     const [test, setTest] = useState(null);
     const location = useLocation();
 
     const queryParams = new URLSearchParams(location.search);
-    const goodsName = queryParams.get('goodsName');
+    const goodsNameParams = queryParams.get('goodsName');
+
     const totalPrice = queryParams.get('totalPrice');
+    const merchantKey = "K/Yp1YrgMPr2FwvMo7Pzvr6F8zhEZpfvrYduZw1U5LXa7LzBUsnii1hnhcWaeIffKCjFjvrotzWAIyBc4+sMPw==";
+    const merchantID = "fittest01m";
+    const ediDate = format(new Date(), 'yyyyMMddHHmmss');
+    const amt = totalPrice;
+    const returnURL = 'http://localhost:8080/test';
+    const goodsName = goodsNameParams;
+    const moid = 'nice_api_test_3.0';
+    const signData = getSignData(ediDate + merchantID + amt + merchantKey).toString();
+    const formRef = useRef(null);
 
     useEffect(() => {
         const handleEvent = (event) => {
@@ -29,6 +41,34 @@ function TestData(props) {
         };
     }, []);
 
+
+
+  
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  
+
+
+  
+    useEffect(() => {
+      detectDeviceAndAssignMethods(isMobile);
+    }, []);
+  
+  
+    function detectDeviceAndAssignMethods(isMobile) {
+      if (isMobile) {
+        // 모바일 환경의 경우
+        formRef.current.action = "https://web.nicepay.co.kr/v3/v3Payment.jsp";
+        formRef.current.acceptCharset = "euc-kr";
+        formRef.current.submit();
+      } 
+    }
+  
+    function getSignData(str) {
+      var encrypted = CryptoJS.SHA256(str);
+      return encrypted;
+    }
+  
+
     return (
         <>
         <div>
@@ -38,14 +78,28 @@ function TestData(props) {
             
              
                     <>
-                        <p>test: {test.paymentInfoData}</p>
+                        <p>test: {test}</p>
                         <p>
                             <span>goodsName: {goodsName}</span>
                             <span>totalPrice: {totalPrice}</span>
                         </p>
                     </>
                 
-            
+        <form
+        name="payForm"
+        method="post"
+        action={returnURL}
+        ref={formRef}
+        acceptCharset="euc-kr">
+          <input type="hidden" name="GoodsName" value={goodsName}/>
+        <input type="hidden" name="Amt" value={amt}/>
+        <input type="hidden" name="MID" value={merchantID}/>
+        <input type="hidden" name="EdiDate" value={ediDate}/>
+        <input type="hidden" name="Moid" value={moid}/>
+        <input type="hidden" name="SignData" value={signData}/>
+        <input type="hidden" name="PayMethod" value="CARD"/>
+        <input type="hidden" name="ReturnURL" value={returnURL}/>
+      </form>
         </div>
         </>
     );
